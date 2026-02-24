99c47a70c2aecd7b61bca94719a2384df6a0c7b3
masudabdullaa <masudabdullaa@gmail.com>
2026-02-24 23:27:18 +0600

    count functionality added and trying to add no job section but its not working

--- tracker.js
+++ tracker.js
@@ -27,83 +27,83 @@ const totalRejected = document.querySelector(".total_rejected");
 
 
 
-mobileFirstInterviewButton.addEventListener("click", function(){
+mobileFirstInterviewButton.addEventListener("click", function () {
     totalInterview.innerHTML++;
     mobileFirstInterviewButton.classList.add("interview_btn");
     mobileFirstRejectButton.classList.remove("reject_btn");
 });
-mobileFirstRejectButton.addEventListener("click", function(){
+mobileFirstRejectButton.addEventListener("click", function () {
     totalRejected.innerHTML++;
     mobileFirstRejectButton.classList.add("reject_btn");
     mobileFirstInterviewButton.classList.remove("interview_btn");
 
 });
-webFlowAgencyInterview.addEventListener("click", function(){
+webFlowAgencyInterview.addEventListener("click", function () {
     totalInterview.innerHTML++;
     webFlowAgencyInterview.classList.add("interview_btn");
     webFlowAgencyReject.classList.remove("reject_btn");
 })
-webFlowAgencyReject.addEventListener("click", function(){
+webFlowAgencyReject.addEventListener("click", function () {
     totalRejected.innerHTML++;
     webFlowAgencyReject.classList.add("reject_btn");
     webFlowAgencyInterview.classList.remove("interview_btn");
 })
-dataVizOlutionsInterview.addEventListener("click", function(){
+dataVizOlutionsInterview.addEventListener("click", function () {
     totalInterview.innerHTML++;
     dataVizOlutionsInterview.classList.add("interview_btn");
     dataVizOlutionsReject.classList.remove("reject_btn");
 })
-dataVizOlutionsReject.addEventListener("click", function(){
+dataVizOlutionsReject.addEventListener("click", function () {
     totalRejected.innerHTML++;
     dataVizOlutionsReject.classList.add("reject_btn");
     dataVizOlutionsInterview.classList.remove("interview_btn");
 })
-cloudFirstIncInterview.addEventListener("click", function(){
+cloudFirstIncInterview.addEventListener("click", function () {
     totalInterview.innerHTML++;
     cloudFirstIncInterview.classList.add("interview_btn");
     cloudFirstIncReject.classList.remove("reject_btn");
 })
-cloudFirstIncReject.addEventListener("click", function(){
+cloudFirstIncReject.addEventListener("click", function () {
     totalRejected.innerHTML++;
     cloudFirstIncReject.classList.add("reject_btn");
     cloudFirstIncInterview.classList.remove("interview_btn");
 })
-innovationLabsInterview.addEventListener("click", function(){
+innovationLabsInterview.addEventListener("click", function () {
     totalInterview.innerHTML++;
     innovationLabsInterview.classList.add("interview_btn");
     innovationLabsReject.classList.remove("reject_btn");
 })
-innovationLabsReject.addEventListener("click", function(){
+innovationLabsReject.addEventListener("click", function () {
     totalRejected.innerHTML++;
     innovationLabsReject.classList.add("reject_btn");
     innovationLabsInterview.classList.remove("interview_btn");
 })
-megaCorpSolutionsInterview.addEventListener("click", function(){
+megaCorpSolutionsInterview.addEventListener("click", function () {
     totalInterview.innerHTML++;
     megaCorpSolutionsInterview.classList.add("interview_btn");
     megaCorpSolutionsReject.classList.remove("reject_btn");
 })
-megaCorpSolutionsReject.addEventListener("click", function(){
+megaCorpSolutionsReject.addEventListener("click", function () {
     totalRejected.innerHTML++;
     megaCorpSolutionsReject.classList.add("reject_btn");
     megaCorpSolutionsInterview.classList.remove("interview_btn");
 })
-startupXYZInterview.addEventListener("click", function(){
+startupXYZInterview.addEventListener("click", function () {
     totalInterview.innerHTML++;
     startupXYZInterview.classList.add("interview_btn");
     startupXYZReject.classList.remove("reject_btn");
 })
-startupXYZReject.addEventListener("click", function(){
+startupXYZReject.addEventListener("click", function () {
     totalRejected.innerHTML++;
     startupXYZReject.classList.add("reject_btn");
     startupXYZInterview.classList.remove("interview_btn");
 })
-techCorpIndustriesInterview.addEventListener("click", function(){
+techCorpIndustriesInterview.addEventListener("click", function () {
     totalInterview.innerHTML++;
     techCorpIndustriesInterview.classList.add("interview_btn");
     techCorpIndustriesReject.classList.remove("reject_btn");
 })
-techCorpIndustriesReject.addEventListener("click", function(){
+techCorpIndustriesReject.addEventListener("click", function () {
     totalRejected.innerHTML++;
     techCorpIndustriesReject.classList.add("reject_btn");
     techCorpIndustriesInterview.classList.remove("interview_btn");
@@ -114,43 +114,60 @@ techCorpIndustriesReject.addEventListener("click", function(){
 document.getElementById("mobile-first-corp-delete")
     .addEventListener("click", function () {
         mobileFirstCorp.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 document.getElementById("webFlow-agency-delete")
     .addEventListener("click", function () {
         webFlowAgency.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 document.getElementById("dataViz-olutions-delete")
     .addEventListener("click", function () {
         dataViOlutions.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 document.getElementById("cloudFirst-inc-delete")
     .addEventListener("click", function () {
         cloudFirstInc.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 document.getElementById("innovation-labs-delete")
     .addEventListener("click", function () {
         innovationLabs.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 document.getElementById("megaCorp-solutions-delete")
     .addEventListener("click", function () {
         megaCorpSolutions.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 document.getElementById("startup-xyz-delete")
     .addEventListener("click", function () {
         startupXYZ.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 document.getElementById("techCorp-industries-delete")
     .addEventListener("click", function () {
         techCorpIndustries.style.display = "none";
+        availableJob.innerHTML--;
+        totalCount.innerHTML--;
     })
 
 
-    const sectionCount = document.getElementById("section-count");
-    const availableJob = document.querySelector(".total_job");
-    const totalCount = document.querySelector(".total_count");
-    const count = sectionCount.childElementCount;
-    availableJob.innerHTML = count;
-    totalCount.innerHTML = count;
-    if(sectionCount.childElementCount == ""){
-        sectionCount.classList.add("no_jobs");
-    }
+const sectionCount = document.getElementById("section-count");
+const availableJob = document.querySelector(".total_job");
+const totalCount = document.querySelector(".total_count");
+const hidden = document.querySelector(".hidden");
+const count = sectionCount.childElementCount;
+availableJob.innerHTML = count;
+totalCount.innerHTML = count;
+if (availableJob.innerHTML == 0 || totalCount.innerHTML == 0) {
+    hidden.style.display = "block";
+}
